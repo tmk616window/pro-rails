5cea3b62c990c0d03abb99b2a34d7c7c565383ae
難波智貴 <nanbatomoki@nanbasatoshitakanoMacBook-Pro.local>
2021-10-08 04:47:12 +0900

    ログインAPI作成

---
+++ config/initializers/cors.rb
@@ -0,0 +1,12 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins "localhost:9001" 
+  
+      resource "*",
+        headers: :any,
+        expose: ["access-token", "expiry", "token-type", "uid", "client"], 
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+        
+    end
+  end
+  
