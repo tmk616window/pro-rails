b139f4d7d30e47e63f0cf7a7a97086c9c53aad0a
難波智貴 <nanbatomoki@nanbasatoshitakanoMacBook-Pro.local>
2021-10-10 05:47:41 +0900

    相談

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "localhost:9001" 
+      origins "*" 
   
       resource "*",
         headers: :any,
